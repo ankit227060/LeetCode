f4fa42e9e86e8565900f237545f42fcfb3de31c9
Ankit <127927126+ankit227060@users.noreply.github.com>
2025-09-20 21:17:32 +0530

    Time: 9 ms (50.19%), Space: 45.2 MB (18.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,23 +1,56 @@
+// import java.util.*;
+
+// class Solution {
+//     public int[] twoSum(int[] nums, int target) {
+//         // Map to store number and its index
+//         Map<Integer, Integer> map = new HashMap<>();
+
+//         for (int i = 0; i < nums.length; i++) {
+//             int complement = target - nums[i];
+
+//             // Check if complement exists in the map
+//             if (map.containsKey(complement)) {
+//                 return new int[] { map.get(complement), i };
+//             }
+
+//             // Otherwise, store current number with its index
+//             map.put(nums[i], i);
+//         }
+
+//         // Since exactly one solution exists, we never reach here
+//         return new int[] {};
+//     }
+// }
+
+
 import java.util.*;
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Map to store number and its index
-        Map<Integer, Integer> map = new HashMap<>();
+        int[][] arr = new int[nums.length][2];
 
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-
-            // Check if complement exists in the map
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
-            }
-
-            // Otherwise, store current number with its index
-            map.put(nums[i], i);
+            arr[i][0] = nums[i];
+            arr[i][1] = i;
         }
 
-        // Since exactly one solution exists, we never reach here
-        return new int[] {};
+        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
+
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = arr[left][0] + arr[right][0];
+            if (sum < target) {
+                left++;
+            } else if (sum > target) {
+                right--;
+            } else {
+                return new int[] { arr[left][1], arr[right][1] };
+            }
+        }
+
+        return new int[0];
     }
 }
+
