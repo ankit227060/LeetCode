444c77bf2e86f4ff47303c51dba7777d39a35266
Ankit <127927126+ankit227060@users.noreply.github.com>
2025-09-22 14:37:13 +0530

    Time: 35 ms (23.95%), Space: 67.4 MB (50.04%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,33 +1,64 @@
-import java.util.*;
+//import java.util.*;
 
-class Solution {
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         int longestLength = 0;
+//         HashMap <Integer, Boolean > newmap  = new HashMap<>();
+//         for (int num:nums){
+//             newmap.put(num,Boolean.FALSE);
+//         }
+
+//         for(int num:nums){
+//             int currentLength = 1;
+
+//             int nextNum = num+1;
+//             while(newmap.containsKey(nextNum) && newmap.get(nextNum) == false){
+//                 currentLength ++ ;
+//                 newmap.put(nextNum,Boolean.TRUE);
+//                 nextNum++;
+//             }
+
+//             int prevNum = num-1;
+//             while(newmap.containsKey(prevNum) && !newmap.get(prevNum)){
+//                 currentLength ++;
+//                 newmap.put(prevNum,Boolean.TRUE);
+//                 prevNum--;
+//             }
+
+//             longestLength = Math.max(longestLength, currentLength);
+//         }
+
+//         return longestLength;
+//     }
+// }
+
+
+
+import java.util.HashSet;
+
+public class Solution {
     public int longestConsecutive(int[] nums) {
-        int longestLength = 0;
-        HashMap <Integer, Boolean > newmap  = new HashMap<>();
-        for (int num:nums){
-            newmap.put(num,Boolean.FALSE);
+        HashSet<Integer> myset = new HashSet<>();
+        int maxsize = 0;
+
+        for (int num : nums) {
+            myset.add(num);
         }
 
-        for(int num:nums){
-            int currentLength = 1;
+        for (int num : myset) {
+            if (!myset.contains(num - 1)) {
+                int current = num;
+                int current_size = 1;
 
-            int nextNum = num+1;
-            while(newmap.containsKey(nextNum) && newmap.get(nextNum) == false){
-                currentLength ++ ;
-                newmap.put(nextNum,Boolean.TRUE);
-                nextNum++;
+                while (myset.contains(current + 1)) {
+                    current++;
+                    current_size++;
+                }
+
+                maxsize = Math.max(maxsize, current_size);
             }
-
-            int prevNum = num-1;
-            while(newmap.containsKey(prevNum) && !newmap.get(prevNum)){
-                currentLength ++;
-                newmap.put(prevNum,Boolean.TRUE);
-                prevNum--;
-            }
-
-            longestLength = Math.max(longestLength, currentLength);
         }
 
-        return longestLength;
+        return maxsize;
     }
 }
