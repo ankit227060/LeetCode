9dfa757ffef5167cf80a97c3f5a2cdbdadeac842
Ankit <127927126+ankit227060@users.noreply.github.com>
2025-10-01 20:26:45 +0530

    Time: 0 ms (100%), Space: 40.8 MB (20.5%) - LeetHub

---
+++ 1642-water-bottles/1642-water-bottles.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int totalDrunk = numBottles;
+        int empty = numBottles;
+
+        while (empty >= numExchange) {
+            int newBottles = empty / numExchange;
+            totalDrunk += newBottles;
+            empty = empty % numExchange + newBottles;
+        }
+
+        return totalDrunk;
+    }
+}
