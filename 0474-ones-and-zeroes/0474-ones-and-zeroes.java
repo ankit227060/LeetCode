7214a153ccc0463e77c8d7ef920908c8fe9bb215
Ankit <127927126+ankit227060@users.noreply.github.com>
2025-11-11 12:30:25 +0530

    Time: 85 ms (18.18%), Space: 43.7 MB (49.23%) - LeetHub

--- 0474-ones-and-zeroes/0474-ones-and-zeroes.java
+++ 0474-ones-and-zeroes/0474-ones-and-zeroes.java
@@ -1,15 +1,28 @@
 class Solution {
-    public int findMaxForm(String[] S, int M, int N) {
-        int[][] dp = new int[M+1][N+1];
-        for (String str : S) {
-            int zeros = 0, ones = 0;
-            for (char c : str.toCharArray())
-                if (c == '0') zeros++;
-                else ones++;
-            for (int i = M; i >= zeros; i--)
-                for (int j = N; j >= ones; j--)
-                    dp[i][j] = Math.max(dp[i][j], dp[i-zeros][j-ones] + 1);
+    private int[] getCnts(String s) {
+        int zeros = 0;
+        int ones = 0;
+        for (char ch : s.toCharArray()) {
+            if (ch == '0') zeros++;
+            else ones++;
         }
-        return dp[M][N];
+        return new int[] {zeros , ones};
+    }
+
+    public int findMaxForm(String[] strs, int m, int n) {
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (String s : strs) {
+            int[] cost = getCnts(s);
+            int cst0 = cost[0];
+            int cst1 = cost[1];
+
+            for (int i = m; i >= cst0; i--) {
+                for (int j = n; j >= cst1; j--) {
+                    dp[i][j] = Math.max(dp[i][j] , 1 + dp[i - cst0][j - cst1]);
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
