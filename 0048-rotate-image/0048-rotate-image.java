d247d2a58c29a79aac63be3704b3052b7e15ec69
Ankit <127927126+ankit227060@users.noreply.github.com>
2026-05-04 06:01:10 +0530

    Time: 0 ms (100%), Space: 44 MB (16.04%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,40 +1,26 @@
-// class Solution {
-//     public void rotate(int[][] matrix) {
-//         int n = matrix.length;
-//         int [][] copy = new int[n][n];
-//         for(int i=0; i<n;i++){
-//             for(int j=0; j<n;j++){
-//                 copy[i][j] =matrix[n-j-1][i];
-//             }
-//         }
-
-//         for(int i =0; i<n;i++){
-//             for (int j =0;j<n;j++){
-//                 matrix[i][j]=copy[i][j];
-//             }
-//         }
-
-//     }
-// }
-
-
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        
-        for(int i=0;i<(n+1)/2;i++){
-            for(int j=0;j<n/2;j++){
-                
-                int temp = matrix[n-1-j][i];
+        int edgeLength = matrix.length;
 
-                matrix[n - 1 - j][i] = matrix[n - 1 - i][n - j - 1];
-                
-                matrix[n - 1 - i][n - j - 1] = matrix[j][n - 1 - i];
+        int top = 0;
+        int bottom = edgeLength - 1;
 
-                matrix[j][n - 1 - i] = matrix[i][j];
-
-                matrix[i][j] = temp;
+        while (top < bottom) {
+            for (int col = 0; col < edgeLength; col++) {
+                int temp = matrix[top][col];
+                matrix[top][col] = matrix[bottom][col];
+                matrix[bottom][col] = temp;
             }
+            top++;
+            bottom--;
         }
+
+        for (int row = 0; row < edgeLength; row++) {
+            for (int col = row + 1; col < edgeLength; col++) {
+                int temp = matrix[row][col];
+                matrix[row][col] = matrix[col][row];
+                matrix[col][row] = temp;
+            }
+        }        
     }
 }
