c13f645131a4ae46e6f26c769fa15fca0be04fd5
Ankit <127927126+ankit227060@users.noreply.github.com>
2026-07-11 23:21:19 +0530

    Time: 12 ms (44.44%), Space: 47.7 MB (39.49%) - LeetHub

--- 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
+++ 2685-count-the-number-of-complete-components/2685-count-the-number-of-complete-components.java
@@ -1,62 +1,43 @@
-import java.util.Collection;
+import java.util.*;
 
 class Solution {
-    class UnionFind {
-        private int[] parents;
-        public UnionFind(int size) {
-            parents = new int[size];
-            for (int i = 0; i < size; ++i) {
-                parents[i] = i;
-            }
+    public int countCompleteComponents(int n, int[][] edges) {
+        List<Integer>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) adj[i] = new ArrayList<>();
+        for (int[] e : edges) {
+            adj[e[0]].add(e[1]);
+            adj[e[1]].add(e[0]);
         }
-        public int find(int node) {
-            int start = node;
-            while (node != parents[node]) {
-                node = parents[node];
-            }
-            return parents[start] = node;
-        }
-        public void connect(int source, int target) {
-            int sourceParent = find(source);
-            int targetParent = find(target);
-            if (sourceParent == targetParent) return;
-            parents[sourceParent] = targetParent;
-        }
-        public Collection<List<Integer>> getComponents() {
-            Map<Integer, List<Integer>> components = new HashMap<>();
-            for (int i = 0; i < parents.length; ++i) {
-                int parent = find(i);
-                if (components.containsKey(parent)) {
-                    components.get(parent).add(i);
-                } else {
-                    components.put(parent, new ArrayList<>(List.of(i)));
+        
+        boolean[] vis = new boolean[n];
+        int res = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (vis[i]) continue;
+            List<Integer> comp = new ArrayList<>();
+            Queue<Integer> q = new LinkedList<>();
+            q.add(i);
+            vis[i] = true;
+            
+            while (!q.isEmpty()) {
+                int curr = q.poll();
+                comp.add(curr);
+                for (int next : adj[curr]) {
+                    if (!vis[next]) {
+                        vis[next] = true;
+                        q.add(next);
+                    }
                 }
             }
-            return components.values();
-        }
-    }
-    public int countCompleteComponents(int n, int[][] edges) {
-        int res = 0;
-        UnionFind uf = new UnionFind(n);
-        int[] degree = new int[n];
-        for (int[] edge : edges) {
-            int source = edge[0];
-            int target = edge[1];
-            uf.connect(source, target);
-            degree[source]++;
-            degree[target]++;
-        }
-        Collection<List<Integer>> components = uf.getComponents();
-        for (List<Integer> component : components) {
-            int expectedDegree = component.size() - 1;
-            boolean completeSubGraph = true;
-            for (int vertice : component) {
-                if (degree[vertice] != expectedDegree) {
-                    completeSubGraph = false;
+            
+            boolean ok = true;
+            for (int node : comp) {
+                if (adj[node].size() != comp.size() - 1) {
+                    ok = false;
                     break;
                 }
             }
-            if (completeSubGraph) res++;
+            if (ok) res++;
         }
         return res;
     }
